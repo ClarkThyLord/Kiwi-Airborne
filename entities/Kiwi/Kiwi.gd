8b62856ba4c0d7069fa162ac913c2bde7437dc5f
ClarkThyLord <ccpotential@gmail.com>
2020-09-19 17:30:41 -0700

    progress on Game
    Clark was here :V

--- entities/Kiwi/Kiwi.gd
+++ entities/Kiwi/Kiwi.gd
@@ -2,27 +2,28 @@ extends KinematicBody2D
 
 
 
+# Refrences
+
+
+
 # Declarations
-export(int) var Height := 0
+signal crashed
 
-export(float) var Speed := 150.0 # For horizontal movement 
-
-export(float) var FallSpeed := 10.0 # For vertical movement
-export(float) var MaxFallSpeed := 150.0 # Max vertical speed
-export(float) var Gravity := 9.8 # Vertical acceleration
+export(float) var Speed := 300.0 # For horizontal movement
 
 
 
 # Core
-func _ready():
-	update()
-
 func _physics_process(delta : float):
 	var direction : Vector2
 	if Input.is_action_pressed("ui_right"): direction += Vector2.RIGHT
+	if Input.is_action_pressed("ui_down") and position.y < 500:
+		direction += Vector2.DOWN
 	if Input.is_action_pressed("ui_left"): direction += Vector2.LEFT
-#	translate(direction * Speed * delta)
-	move_and_collide(direction * Speed * delta)
-
-func _draw():
-	draw_circle(Vector2.ZERO, 32, Color.green)
+	if Input.is_action_pressed("ui_up") and position.y > 100:
+		direction += Vector2.UP
+	var hit = move_and_collide(direction.normalized() * Speed * delta)
+	
+	if is_instance_valid(hit):
+		print("crashed")
+		emit_signal("crashed")
--- scenes/Game/Game.gd
+++ scenes/Game/Game.gd
@@ -8,18 +8,27 @@ const Rock := preload("res://objects/rock/Rock.tscn")
 
 
 # Refrences
+onready var Kiwi := get_node("Kiwi")
+
+onready var Stats := get_node("CanvasLayer/HUD/Stats")
 
 
 
 # Declarations
+export(float, 0.0, 250.0) var Speed := 10.0
+export(float, 0.0, 100.0) var Gravity := 9.8
+export(float, 0.0, 1000.0) var Flight := 0.0
+
+
 var Spawns = [Vector2(120, 750), Vector2(904, 750)]
 
 
 
 # Core
 func _process(delta : float) -> void:
-	$Walls/Texture.region_rect.position.y += 1
-	$Background/Mountains.region_rect.position.y += 0.01
+	$Background/Mountains.region_rect.position.y += (Speed / 1000) * delta
+	$Walls/Texture.region_rect.position.y += (Speed / 2) * delta
+	
 	if $Objects.get_child_count() < 10:
 		var rock = Rock.instance()
 		$Objects.add_child(rock)
@@ -31,4 +40,13 @@ func _process(delta : float) -> void:
 			rock.scale.x *= -1
 			rock.position = Spawns[1]
 	for object in $Objects.get_children():
-		object.position.y -= 8
+		object.position.y -= Speed * 6 * delta
+	
+	Flight += Speed * delta
+	Speed = clamp(Speed + Gravity * delta, 0.0, 120)
+	
+	Stats.text = PoolStringArray([
+		"FEATHERS : " + str(0) + "\n",
+		"FLIGHT : " + str(Flight) + "\n",
+		"SPEED : " + str(Speed) + " \\ " + str(120)
+	]).join("")
