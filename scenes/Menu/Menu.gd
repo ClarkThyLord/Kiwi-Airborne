08dd7d240172a7c85d1a4ccfe6580e181b87010e
ClarkThyLord <ccpotential@gmail.com>
2020-09-20 08:07:26 -0700

    adding Menu
    Clark was here :V

---
+++ scenes/Menu/Menu.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+
+
+# Refrences
+onready var Content := get_node("Content")
+
+
+
+# Core
+func _ready():
+	Content.visible = false
+
+
+func _process(delta):
+	if Input.is_action_just_released("ui_cancel"):
+		close() if $Content.visible else open()
+
+
+func open() -> void:
+	Content.visible = true
+	get_tree().paused = true
+
+func close() -> void:
+	Content.visible = false
+	get_tree().paused = false
---
+++ scenes/Start/Start.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+# Core
+func _on_Play_pressed():
+	pass # Replace with function body.
+
+func _on_Help_pressed():
+	get_node("/root/Menu").open()
+
+func _on_Settings_pressed():
+	get_node("/root/Menu").open()
